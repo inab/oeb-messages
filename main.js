84a4e599b479e5f2313e82c0fc562a63c35b29d8
Jessica F. Martinez <jessicafndez@gmail.com>
2024-08-01 12:36:56 +0200

    UPDATE README file

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import CustomMessage from './CustomMessage.js'
 
-// For DEV mode
+// JUST For DEV mode
 document.querySelector('#oeb-message').innerHTML = `
     <custom-message url="https://raw.githubusercontent.com/inab/oeb-messages/main/demo/message.json"></custom-message>
   `
